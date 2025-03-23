03588982eb76303184e952186d88c48f9b62e6bb
Sam Dickinson <sam@sdickinson.com>
2025-03-23 18:27:46 +1100

    Added UnionResolver and OmitMethods types

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,8 @@ export type Nullish = null | undefined;
 export type Nullable<T> = T | Nullish;
 export type ValueOf<T> = T[keyof T]; // Union of all possible values in an object
 export type Falsy = false | '' | 0 | null | undefined;
+export type UnionResolver<T> = T extends infer U ? { type: U }[] : never;
+export type OmitMethods<T> = Pick<
+  T,
+  { [K in keyof T]: T[K] extends Function ? never : K }[keyof T]
+>;
