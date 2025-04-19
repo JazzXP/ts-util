bd70c1fc821b22758f85f0df7f03651e77849a90
Sam Dickinson <sam@sdickinson.com>
2025-04-20 09:35:02 +1000

    Updated comment

--- src/types.ts
+++ src/types.ts
@@ -10,4 +10,5 @@ export type OmitMethods<T> = Pick<
 // By declaring a unique symbol, we create a distinct marker in TypeScript.
 declare const __brand: unique symbol;
 // Define a Branded type that combines a base type with a brand
+// eg. Branded<string, "Hash"> then you can specify an input is required to be a hashed string
 export type Branded<Type, Brand> = Type & { readonly [__brand]: Brand };
